76a0d30ab790bc0383e3a04327e550aa57f480be
Norbert Hunyadi <norberthunyadi@gmail.com>
2018-09-01 19:58:14 +0300

    image gallery support

--- assets/js/scripts/script.js
+++ assets/js/scripts/script.js
@@ -194,6 +194,19 @@ $(document).ready(function () {
   //   s.src = '//' + disqus_shortname + '.disqus.com/count.js';
   //   (document.getElementsByTagName('BODY')[0]).appendChild(s);
   // }());
+
+
+  // =============
+  // Image Gallery
+  // =============
+  var images = document.querySelectorAll('.kg-gallery-image img');
+  images.forEach(function (image) {
+      var container = image.closest('.kg-gallery-image');
+      var width = image.attributes.width.value;
+      var height = image.attributes.height.value;
+      var ratio = width / height;
+      container.style.flex = ratio + ' 1 0%';
+  })
 });
 
 var pagination = 0;
